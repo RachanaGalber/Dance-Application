863bdb0384ee195ab4842b67999a321ae40b1c60
iknowimhere <rachana12345@gmail.com>
2023-10-16 16:27:28 +0530

    first commit

---
+++ src/Menu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Menu=()=>{
+    return(
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Signup</li>
+            </ul>
+        </div>
+    )
+}
+export default Menu
---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from "./Logo"
+import Menu from "./Menu"
+
+const Navbar=()=>{
+    return(
+        <section >
+           <div id='nav'>
+           <Logo/>
+           <Menu/>
+           </div>
+        </section>
+
+    )
+}
+export default Navbar
